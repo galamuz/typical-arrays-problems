f21f9fa38e3220a658497736bb38f510cebbd088
Galina Muzychenko <muzychenko.galina@gmail.com>
2020-09-29 22:45:05 +0300

    init: start task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
+
+  if (array && array.length) {
+    return Math.min.apply(null, array)
+  }
   return 0;
 }
-
 exports.max = function max (array) {
+  if (array && array.length) {
+    return Math.max.apply(null, array)
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+
+  if (array && array.length) {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
   return 0;
 }
